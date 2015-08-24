6eed903a4e07fe4af8d578add89d9a8572692ac9
Debra <devra29@yahoo.com>
2015-08-23 20:38:18 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,43 @@
+##Project 5
+
+Resources
+
+Knockout - http://learn.knockoutjs.com/#/?tutorial=loadingsaving
+
+rainy mood sound - http://www.rainymood.com/
+
+beginners guide to knockout - http://www.sitepoint.com/beginners-guide-to-knockoutjs-part-1/
+
+getting started with knockout - https://www.develop.com/knockoutjavascript
+
+good example of Project 5 - https://www.linkedin.com/in/bahalperin
+
+Google Maps Basics - http://www.w3schools.com/googleapi/google_maps_basic.asp
+
+How to use google maps api - http://code.tutsplus.com/articles/this-is-how-you-use-the-google-maps-api-screencast--net-1187
+
+google api info - https://developers.google.com/maps/documentation/javascript/tutorial#api_key
+
+responsive google map - http://stackoverflow.com/questions/15421369/responsive-google-map
+
+
+****http://stackoverflow.com/questions/18962700/bootstrap-responsive-div-display - responsive div in bootstrap
+
+
+*** helped me with map - http://cheatsheet.zemplate.com/2-uncategorised/180-google-maps-api-v3-simple-example
+
+***helped me with resizing and keeping center of map
+http://hsmoore.com/keep-google-map-v3-centered-when-browser-is-resized/
+
+*** google markers
+https://developers.google.com/maps/documentation/javascript/markers
+
+
+centering the boulder marker - http://hsmoore.com/keep-google-map-v3-centered-when-browser-is-resized/
+
+cleaning up js/html/css - http://www.dirtymarkup.com/
+
+
+open weather api - http://openweathermap.org/examples
+
+
---
+++ js/main.js
@@ -0,0 +1,107 @@
+
+// Define your locations: HTML content for the info window, latitude, longitude
+var locations = [
+    ['<h4>The Laughing Goat Coffeehouse</h4>', 40.019604, -105.273141],
+    ['<h4>Ozo Cafe</h4>', 40.014190, -105.294955],
+    ['<h4>Espresso Roma</h4>', 40.007397, -105.276331],
+    ['<h4>Trident Booksellers and Cafe</h4>', 40.016968, -105.282887],
+    ['<h4>Red Rock Cafe</h4>', 40.034895, -105.260033]
+];
+// Setup the different icons and shadows
+var iconURLPrefix = 'http://maps.google.com/mapfiles/ms/icons/';
+var icons = [
+    iconURLPrefix + 'red-dot.png',
+    iconURLPrefix + 'green-dot.png',
+    iconURLPrefix + 'blue-dot.png',
+    iconURLPrefix + 'orange-dot.png',
+    iconURLPrefix + 'yellow-dot.png'
+]
+var iconsLength = icons.length;
+var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: new google.maps.LatLng(40.010786, -105.274274),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControl: false,
+    streetViewControl: false,
+    panControl: false,
+    zoomControlOptions: {
+        position: google.maps.ControlPosition.LEFT_BOTTOM
+    }
+});
+var infowindow = new google.maps.InfoWindow({
+    maxWidth: 160
+});
+var markers = new Array();
+var iconCounter = 0;
+// Add the markers and infowindows to the map
+for (var i = 0; i < locations.length; i++) {
+    var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][
+            2
+        ]),
+        map: map,
+        icon: icons[iconCounter]
+    });
+    markers.push(marker);
+    google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+            infowindow.setContent(locations[i][0]);
+            infowindow.open(map, marker);
+        }
+    })(marker, i));
+    iconCounter++;
+    // We only have a limited number of possible icon colors, so we may have to restart the counter
+    if (iconCounter >= iconsLength) {
+        iconCounter = 0;
+    }
+}
+//Binds for list of coffee shops
+//Will let the user know when Google Maps fails to load.
+//This does not work
+function failedToLoad() {
+    $('#map').html("Google Maps Failed to Load");
+}
+
+  $(function() {
+    var coffee = [
+      {name:"Red Rock Cafe"},
+      {name:"The Laughing Goat Coffeehouse"},
+      {name:"Trident Booksellers and Cafe"},
+	  {name:"Expresso Roma"},
+	  {name:"Ozo Cafe"},
+    ];
+
+    var viewModel = {
+      coffee: ko.observableArray(coffee),
+
+      query: ko.observable(''),
+
+      //This doesn't work through viewModel.query line
+      search: function(value) {
+        viewModel.coffee.removeAll();
+        for(var x in coffee) {
+          if(coffee[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+            viewModel.coffee.push(coffee[x]);
+          }
+        }
+      }
+    };
+
+    viewModel.query.subscribe(viewModel.search);
+
+    ko.applyBindings(viewModel);
+  });
+  
+ 
+function b() {
+                var apiKey = '5222045741e8792041d7a9f8afc89168';
+                var url = 'https://api.forecast.io/forecast/';
+                var lati = 40.0149900;
+                var longi = -105.2705500;
+                var data;
+                $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?",
+                function(data) {
+                $('#weather').html('Temp: ' + data.currently.temperature + ' ' + 'degrees');
+                });
+                }        
+               
