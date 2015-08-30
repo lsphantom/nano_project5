56f4565942c4fb29aad46a8a6b8b194ea08b309d
Debra <devra29@yahoo.com>
2015-08-30 16:09:46 -0600

    updates to js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-
-// Define your locations: HTML content for the info window, latitude, longitude
+//Model
+// Define your locations with GoogleMaps: HTML content for the info window, latitude, longitude
 var locations = [
     ['<h4>The Laughing Goat Coffeehouse</h4>', 40.019604, -105.273141],
     ['<h4>Ozo Cafe</h4>', 40.014190, -105.294955],
@@ -7,6 +7,20 @@ var locations = [
     ['<h4>Trident Booksellers and Cafe</h4>', 40.016968, -105.282887],
     ['<h4>Red Rock Cafe</h4>', 40.034895, -105.260033]
 ];
+//Defines Open Weather Map info 
+function b() {
+        var apiKey = '5222045741e8792041d7a9f8afc89168';
+        var url = 'https://api.forecast.io/forecast/';
+        var lati = 40.0149900;
+        var longi = -105.2705500;
+        var data;
+        $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?",
+            function(data) {
+                $('#weather').html('Temp: ' + data.currently.temperature +
+                    ' ' + 'degrees');
+            });
+    }
+//ViewModel
 // Setup the different icons and shadows
 var iconURLPrefix = 'http://maps.google.com/mapfiles/ms/icons/';
 var icons = [
@@ -28,11 +42,6 @@ var map = new google.maps.Map(document.getElementById('map'), {
         position: google.maps.ControlPosition.LEFT_BOTTOM
     }
 });
-
- // Will let the user know when Google Maps fails to load.
-   function failedToLoad() {
-     $('#map').html("Google Maps Failed to Load");
- };
 var infowindow = new google.maps.InfoWindow({
     maxWidth: 160
 });
@@ -59,52 +68,38 @@ for (var i = 0; i < locations.length; i++) {
     if (iconCounter >= iconsLength) {
         iconCounter = 0;
     }
-
-
-
-} 
- 
-function b() {
-                var apiKey = '5222045741e8792041d7a9f8afc89168';
-                var url = 'https://api.forecast.io/forecast/';
-                var lati = 40.0149900;
-                var longi = -105.2705500;
-                var data;
-                $.getJSON(url + apiKey + "/" + lati + "," + longi + "?callback=?",
-                function(data) {
-                $('#weather').html('Temp: ' + data.currently.temperature + ' ' + 'degrees');
-                });
-                }  
-
+}
 $(function() {
-    var shops = [
-      {name:"Red Rock Cafe"},
-      {name:"The Laughing Goat Coffeehouse"},
-      {name:"Trident Booksellers and Cafe"},
-    {name:"Expresso Roma"},
-    {name:"Ozo Cafe"},
-    ];
-
+    var shops = [{
+        name: "Red Rock Cafe"
+    }, {
+        name: "The Laughing Goat Coffeehouse"
+    }, {
+        name: "Trident Booksellers and Cafe"
+    }, {
+        name: "Expresso Roma"
+    }, {
+        name: "Ozo Cafe"
+    }, ];
     var viewModel = {
-      shops: ko.observableArray(shops),
-
-      query: ko.observable(''),
-
-      search: function(value) {
-        viewModel.shops.removeAll();
-        for(var x in shops) {
-          if(shops[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-            viewModel.shops.push(shops[x]);
-          }
+        shops: ko.observableArray(shops),
+        query: ko.observable(''),
+        search: function(value) {
+            viewModel.shops.removeAll();
+            for (var x in shops) {
+                if (shops[x].name.toLowerCase().indexOf(value.toLowerCase()) >=
+                    0) {
+                    viewModel.shops.push(shops[x]);
+                }
+            }
         }
-      }
     };
-
     viewModel.query.subscribe(viewModel.search);
-
     ko.applyBindings(viewModel);
-  });
+});
 
- 
-                      
-               
+//View
+// Will let the user know when Google Maps fails to load.
+function failedToLoad() {
+    $('#map').html("Google Maps Failed to Load");
+}
